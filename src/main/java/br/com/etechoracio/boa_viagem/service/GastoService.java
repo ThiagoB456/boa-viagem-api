1911035b2904968fd6ed9d27e6849a31a62b6c56
Ana-Kaleb <Ana-Kaleb@test.com>
2022-05-09 08:26:41 -0300

    Agora sim service

--- src/main/java/br/com/etechoracio/boa_viagem/service/GastoService.java
+++ src/main/java/br/com/etechoracio/boa_viagem/service/GastoService.java
@@ -1,5 +1,54 @@
 package br.com.etechoracio.boa_viagem.service;
 
-public class GastoService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.etechoracio.boa_viagem.entity.Gasto;
+import br.com.etechoracio.boa_viagem.repository.GastoRepository;
+
+public class GastoService {
+	@Autowired
+	private GastoRepository repository;
+	
+	
+	public List<Gasto> listarTodos(){
+	return repository.findAll();
+	}
+	
+	
+	public Optional<Gasto> buscarPorID( long id) {
+		return repository.findById(id); 
+			
+	}
+		
+	
+	public boolean deletarID( long id) {
+		boolean existe = repository.existsById(id);
+		if(existe) {
+			 repository.deleteById(id);		 
+		} 
+        	return existe;
+	}
+	
+	
+	public Gasto inserir( Gasto obj) {
+ 		return repository.save(obj);
+ 		 
+ 	
+	}
+	
+	public Optional<Gasto> atualizar( Long id, Gasto gasto) {
+		boolean existe = repository.existsById(id);
+		
+		if(!existe) {
+			 return Optional.empty();
+			 
+		}
+		
+		return Optional.of(repository.save(gasto));
+		
+	}
+	
 }
